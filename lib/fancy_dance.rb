4397013973bb0c2ef671b4c72db602e5a18358d1
Roberto Medina <robby.medina562@gmail.com>
2019-09-30 01:53:11 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -16,7 +16,7 @@ module FancyDance
   
     module ClassMethods
       def metadata
-        "This class produces objects that love to dance"
+        "This class produces objects that love to dance."
       end
     end
   end
